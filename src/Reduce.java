68cc125fbdd613a82df545d0d40a5b3db4b87306
Saadq817 <squreshi7170@gmail.com>
2025-05-15 13:19:10 -0400

    created Reduce class and added a main method

--- src/Reduce.java
+++ src/Reduce.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
-
 public class Reduce {
+    public static void main(String[] args) {
+        int start = 100;
+        int end = 0;
+        int steps = 0;
+        while (start != end){
+            if (start % 2 == 0){
+                start = start / 2;
+                steps++;
+
+            } else {
+                start = start - 1;
+                steps++;
+            }
+
+        }
+        System.out.println(steps);
+    }
 }
